f6da3777c958ae2900b5ea8a7862bed32d8a5362
Yevhwnii <mirandoos3223@gmail.com>
2021-03-06 20:46:25 +0100

    Login page enabled

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import * as ROUTES from './constants/routes';
+
+const Login = lazy(() => import('./pages/Login/Login'));
 
 const App: React.FC = () => {
-  return <div className='App'></div>;
+  return (
+    <Router>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Switch>
+          <Route path={ROUTES.LOGIN}>
+            <Login />
+          </Route>
+        </Switch>
+      </Suspense>
+    </Router>
+  );
 };
 
 export default App;
