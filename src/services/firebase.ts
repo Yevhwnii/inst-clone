a4d0c18b58795b02f1d44d01ee3ad6758b4e1f9f
Yevhwnii <mirandoos3223@gmail.com>
2021-03-09 12:53:32 +0100

    firebase services

---
+++ src/services/firebase.ts
@@ -0,0 +1,16 @@
+import { firebase, FieldValue } from '../lib/firebase';
+import { collection } from 'rxfire/firestore';
+import { map } from 'rxjs/operators';
+
+export const doesUserExists = (username: string) => {
+  console.log('Here');
+
+  const db = firebase.firestore();
+  const userCollectionRef = db
+    .collection('users')
+    .where('username', '==', username);
+
+  return collection(userCollectionRef).pipe(
+    map((user) => (user.length > 0 ? true : false))
+  );
+};
