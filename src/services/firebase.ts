c66b42aae541479b0f4f6d3f3a65d9b9cee2950f
Yevhwnii <mirandoos3223@gmail.com>
2021-03-10 19:38:15 +0100

    getUserById funcionality

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -12,3 +12,18 @@ export const doesUserExists = (username: string) => {
     map((user) => (user.length > 0 ? true : false))
   );
 };
+
+export const getUserById = async (userId: string) => {
+  const db = firebase.firestore();
+  const result = await db
+    .collection('users')
+    .where('userId', '==', userId)
+    .get();
+
+  const [user] = result.docs.map((item) => ({
+    ...item.data(),
+    docId: item.id,
+  }));
+
+  return user;
+};
