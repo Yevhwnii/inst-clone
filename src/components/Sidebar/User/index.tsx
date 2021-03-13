00f0e8d5714e23eceb4f2354956af767d7f3ba37
Yevhwnii <mirandoos3223@gmail.com>
2021-03-13 18:22:25 +0100

    user component in the sidebar

---
+++ src/components/Sidebar/User/index.tsx
@@ -0,0 +1,34 @@
+import React, { memo } from 'react';
+import Skeleton from 'react-loading-skeleton';
+import { Link } from 'react-router-dom';
+
+interface UserProps {
+  username: string;
+  fullName: string;
+}
+
+const User: React.FC<UserProps> = ({ username, fullName }) => {
+  return (
+    <>
+      {!username || !fullName ? (
+        <Skeleton count={1} height={61} />
+      ) : (
+        <Link to={`/p/${username}`} className='grid grid-cols-4 gap-4 mb-6'>
+          <div className='flex items-center justify-between col-span-1'>
+            <img
+              src={`/images/avatars/${username}.jpg`}
+              alt='User avatar'
+              className='rounded-full w-16 flex mr-3'
+            />
+          </div>
+          <div className='col-span-3'>
+            <p className='font-bold text-sm'>{username}</p>
+            <p className='text-sm'>{fullName}</p>
+          </div>
+        </Link>
+      )}
+    </>
+  );
+};
+
+export default memo(User);
