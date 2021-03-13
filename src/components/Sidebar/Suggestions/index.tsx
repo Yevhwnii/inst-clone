1ea6d87f2d6e15f057eb453aade79dd0a3c7119c
Yevhwnii <mirandoos3223@gmail.com>
2021-03-13 18:22:03 +0100

    Suggestions component

---
+++ src/components/Sidebar/Suggestions/index.tsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react';
+import Skeleton from 'react-loading-skeleton';
+
+import { getSuggestedProfiles } from '../../../services/firebase';
+import SuggestedProfile from './SuggestedProfile';
+
+interface SuggestionProps {
+  userId: string;
+  following: [];
+  loggedInUserDocId: string;
+}
+
+const Suggestions: React.FC<SuggestionProps> = ({
+  userId,
+  following,
+  loggedInUserDocId,
+}) => {
+  const [profiles, setProfiles] = useState<any>(null);
+
+  useEffect(() => {
+    const suggestedProfiles = async () => {
+      const response = await getSuggestedProfiles(userId, following);
+      setProfiles(response);
+    };
+
+    if (userId) {
+      suggestedProfiles();
+    }
+  }, [userId, following]);
+
+  return !profiles ? (
+    <Skeleton count={1} height={150} className='mt-5' />
+  ) : profiles.length > 0 ? (
+    <div className='rounded flex flex-col'>
+      <div className='text-sm flex items-center align-items justify-between mb-2'>
+        <p className='font-bold text-gray-base'>Suggestions for you</p>
+      </div>
+      <div className='mt-4 grid gap-5'>
+        {profiles.map((profile: any) => (
+          <SuggestedProfile
+            key={profile.docId}
+            userDocId={profile.docId}
+            username={profile.username}
+            profileId={profile.userId}
+            userId={userId}
+            loggedInUserDocId={loggedInUserDocId}
+          />
+        ))}
+      </div>
+    </div>
+  ) : null;
+};
+
+export default Suggestions;
