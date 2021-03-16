28231633f7390ec777a9c7ea4f3dc44f9a6be234
Yevhwnii <mirandoos3223@gmail.com>
2021-03-16 21:47:14 +0100

    types + comments

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,7 +1,7 @@
 import { firebase, FieldValue } from '../lib/firebase';
 import { collection } from 'rxfire/firestore';
 import { map } from 'rxjs/operators';
-import { user } from 'rxfire/auth';
+import { IPhoto, IUser } from './types';
 
 export const doesUserExists = (username: string) => {
   const db = firebase.firestore();
@@ -22,27 +22,38 @@ export const getUserById = async (userId: string) => {
     .get();
 
   const [user] = result.docs.map((item) => ({
-    ...item.data(),
+    ...(item.data() as IUser),
     docId: item.id,
   }));
 
   return user;
 };
 
-export const getSuggestedProfiles = async (userId: string, following: []) => {
+export const getSuggestedProfiles = async (
+  userId: string,
+  following: [string]
+) => {
   const db = firebase.firestore();
   const result = await db.collection('users').limit(10).get();
 
   // filter out your profile and profiles that are already being followed from result
   return result.docs
-    .map((user) => ({ ...user.data(), docId: user.id }))
+    .map((user) => ({ ...(user.data() as IUser), docId: user.id }))
     .filter(
       (profile) =>
-        // @ts-ignore
-        profile.userId !== userId && !following.includes(profile.userId)
+        profile.userId !== userId &&
+        !following.includes(profile.userId as never)
     );
 };
 
+// --------------------------------------------------
+// FOLLOWERS SECTION
+
+// When follow requested, two steps should be taken:
+// 1. Update the "following" array of current logged in user
+// 2. Update the "followers" array of user that is requested to be followed
+
+// Executes action described in step 1
 export const updateLoggedInUserFollowing = async (
   loggedInUserDocId: string, // currently logged in user document id
   profileId: string, // the user that currently logged in user requested to follow
@@ -59,6 +70,7 @@ export const updateLoggedInUserFollowing = async (
     });
 };
 
+// Executes action described in step 2
 export const updateFollowedUserFollowers = async (
   targetedUserDocId: string,
   loggedInUserId: string,
@@ -73,4 +85,40 @@ export const updateFollowedUserFollowers = async (
         ? FieldValue.arrayRemove(loggedInUserId)
         : FieldValue.arrayUnion(loggedInUserId),
     });
+};
+
+// --------------------------------------------------
+// PHOTOS SECTION
+
+// Get photos of users which currently logged in user is following
+export const getPhotos = async (userId: string, following: [string]) => {
+  // Return all photos, where userId in photo document is in array of "following" of the current logged in user
+  const result = await firebase
+    .firestore()
+    .collection('photos')
+    .where('userId', 'in', following)
+    .get();
+  // Map response from firebase into array of object
+  const userFollowedPhotos = result.docs.map((photo) => ({
+    ...(photo.data() as IPhoto),
+    docId: photo.id,
+  }));
+
+  // Populate array of photos with information whether current logged in user (LUI) liked the photo
+  const photosWithUserDetails = await Promise.all(
+    // Map through all photos
+    userFollowedPhotos.map(async (photo) => {
+      let userLikedPhoto = false;
+      // Check if LUI has liked the photo
+      if (photo.likes.includes(userId as never)) {
+        userLikedPhoto = true;
+      }
+      // User that posted the photo
+      const user = await getUserById(photo.userId);
+      const { username } = user;
+      return { username, ...photo, userLikedPhoto };
+    })
+  );
+
+  return photosWithUserDetails;
 };
