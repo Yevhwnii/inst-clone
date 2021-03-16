302f6c3d6d39136e6cedf2e9e19dd468b4a339d0
Yevhwnii <mirandoos3223@gmail.com>
2021-03-16 21:46:11 +0100

    corrected useAuth hook

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -3,13 +3,12 @@ import Firebase from 'firebase/app';
 import { user } from 'rxfire/auth';
 
 import FirebaseContext from '../context/firebase';
-import { getUserById } from '../services/firebase';
 
+// Returns logged in user object
 const useAuth = () => {
   const [authUser, setAuthUser] = useState<Firebase.User | null>(
     JSON.parse(localStorage.getItem('authUser') || 'null')
   );
-  const [userDoc, setUserDoc] = useState<any>(null);
   const { firebase } = useContext(FirebaseContext);
 
   useEffect(() => {
@@ -17,11 +16,9 @@ const useAuth = () => {
     const subscription = user(auth!).subscribe(async (u) => {
       if (u) {
         setAuthUser(u);
-        setUserDoc(await getUserById(u.uid));
         localStorage.setItem('authUser', JSON.stringify(u));
       } else {
         setAuthUser(null);
-        setUserDoc(null);
         localStorage.removeItem('authUser');
       }
     });
@@ -29,7 +26,7 @@ const useAuth = () => {
     return () => subscription.unsubscribe();
   }, [firebase]);
 
-  return [authUser, userDoc] as const;
+  return [authUser] as const;
 };
 
 export default useAuth;
