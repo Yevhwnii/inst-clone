e503fb743484377fd9ffea00d2b2285dcac41f9c
Yevhwnii <mirandoos3223@gmail.com>
2021-03-17 21:43:38 +0100

    updated logic of use photos hook

--- src/hooks/usePhotos.ts
+++ src/hooks/usePhotos.ts
@@ -2,23 +2,30 @@ import { useEffect, useState } from 'react';
 import { getPhotos } from '../services/firebase';
 import { IPhoto, IUser } from '../services/types';
 
+export interface IPhotoModified extends IPhoto {
+  username: string;
+  userLikedPhoto: boolean;
+}
+
 const usePhotos = (user: IUser) => {
-  const [photos, setPhotos] = useState<IPhoto[] | null>(null);
+  const [photos, setPhotos] = useState<IPhotoModified[] | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
     const getTimelinePhotos = async () => {
       // array of users(ids) which are followed by logged in user --- example: ["WgfuSUrf", "SydefaSDdgfag"]
-      let followedUserPhotos = [];
-      // Check if user follows anyone
-      if (user.following.length > 0) {
-        followedUserPhotos = await getPhotos(user.userId, user.following);
+      // Check if user follows anyone and it is now a default object I created
+      if (user.following.length > 0 && user.following[0] !== '') {
+        const followedUserPhotos = await getPhotos(user.userId, user.following);
+        followedUserPhotos.sort((a, b) => b.dateCreated - a.dateCreated);
         setPhotos(followedUserPhotos);
+        setLoading(false);
       }
     };
     getTimelinePhotos();
-  }, [user.userId, user.following]);
+  }, [user]);
 
-  return { photos };
+  return { photos, loading };
 };
 
 export default usePhotos;
